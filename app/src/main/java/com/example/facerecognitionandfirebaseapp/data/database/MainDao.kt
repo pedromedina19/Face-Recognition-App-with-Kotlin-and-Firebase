745c79442349cbaf88e0ea5643b3801a77583e60
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-28 18:28:50 -0300

    Create MainDao for database

---
+++ app/src/main/java/com/example/facerecognitionandfirebaseapp/data/database/MainDao.kt
@@ -0,0 +1,27 @@
+package com.example.facerecognitionandfirebaseapp.data.database
+
+import androidx.room.*
+import com.example.facerecognitionandfirebaseapp.data.model.FaceInfo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MainDao {
+    @Query("DELETE FROM FaceInfo")
+    suspend fun clear()
+
+    @Query("DELETE FROM FaceInfo WHERE id = :id")
+    suspend fun delete(id: Int)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(data: FaceInfo)
+
+    @Query("SELECT * FROM FaceInfo WHERE id = :id")
+    fun face(id: Int): Flow<FaceInfo>
+
+    @Query("SELECT * FROM FaceInfo ")
+    fun faces(): Flow<List<FaceInfo>>
+
+    @Query("SELECT * FROM FaceInfo ")
+    suspend fun faceList(): List<FaceInfo>
+
+}
