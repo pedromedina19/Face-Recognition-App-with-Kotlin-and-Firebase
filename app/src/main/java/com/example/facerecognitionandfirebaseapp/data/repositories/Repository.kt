3bed4bde51f2a68dcdfe62246d5c41d89a8215fd
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-30 16:12:31 -0300

    Flow face info from db

--- app/src/main/java/com/example/facerecognitionandfirebaseapp/data/repositories/Repository.kt
+++ app/src/main/java/com/example/facerecognitionandfirebaseapp/data/repositories/Repository.kt
@@ -2,7 +2,16 @@ package com.example.facerecognitionandfirebaseapp.data.repositories
 
 import android.app.Application
 import com.example.facerecognitionandfirebaseapp.data.database.MainDatabase
+import com.example.facerecognitionandfirebaseapp.data.model.FaceInfo
+import com.example.facerecognitionandfirebaseapp.data.model.ProcessedImage
+import com.example.facerecognitionandfirebaseapp.lib.AiModel.recognizeFace
+import com.example.facerecognitionandfirebaseapp.lib.FileUtils.writeBitmap
+import com.example.facerecognitionandfirebaseapp.lib.LOG
+import kotlinx.coroutines.flow.Flow
 
 class Repository(private val app: Application, private val db: MainDatabase) {
+    // Flow of face information from the database
+    val faces: Flow<List<FaceInfo>> = db.dao.faces()
     
+
 }
