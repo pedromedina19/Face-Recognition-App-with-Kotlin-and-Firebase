93ebb290bd2771935aae91bc1be84052381039fb
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-30 15:04:58 -0300

    Implementing image analyser function

--- app/src/main/java/com/example/facerecognitionandfirebaseapp/data/repositories/Repository.kt
+++ app/src/main/java/com/example/facerecognitionandfirebaseapp/data/repositories/Repository.kt
@@ -1,7 +1,10 @@
 package com.example.facerecognitionandfirebaseapp.data.repositories
 
 import android.app.Application
+import android.graphics.Paint
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.lifecycle.ProcessCameraProvider
 import com.example.facerecognitionandfirebaseapp.data.database.MainDatabase
 import com.example.facerecognitionandfirebaseapp.data.model.FaceInfo
@@ -9,7 +12,9 @@ import com.example.facerecognitionandfirebaseapp.data.model.ProcessedImage
 import com.example.facerecognitionandfirebaseapp.lib.AiModel.recognizeFace
 import com.example.facerecognitionandfirebaseapp.lib.FileUtils.writeBitmap
 import com.example.facerecognitionandfirebaseapp.lib.LOG
+import com.example.facerecognitionandfirebaseapp.lib.MediaUtils.bitmap
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
@@ -88,6 +93,31 @@ class Repository(private val app: Application, private val db: MainDatabase) {
 
     // Select camera based on lens facing
     fun cameraSelector(lensFacing: Int): CameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+    // Configure image analysis for camera
+    fun imageAnalysis(lensFacing: Int, paint: Paint, onData: (Result<ProcessedImage>) -> Unit): ImageAnalysis {
+        val imageAnalyzer: ImageAnalysis.Analyzer = imageAnalyzer(lensFacing, paint, onData)
+        val imageAnalysis = ImageAnalysis.Builder().apply {
+            setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_YUV_420_888)
+        }.build()
+        imageAnalysis.setAnalyzer(cameraExecutor, imageAnalyzer)
+        return imageAnalysis
+    }
+    // Analyzer for processing camera images
+    fun imageAnalyzer(lensFacing: Int, paint: Paint, onFaceInfo: (Result<ProcessedImage>) -> Unit): ImageAnalysis.Analyzer =
+        ImageAnalysis.Analyzer { imageProxy ->
+            runCatching {
+                @ExperimentalGetImage
+                val mediaImage = imageProxy.image ?: throw Throwable("Unable to get Media Image")
+                val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                val bitmap = imageProxy.bitmap.getOrNull() ?: throw Throwable("Unable to get Bitmap")
+                // Process image for face detection
+                faceDetector.process(image)
+                    .addOnSuccessListener(cameraExecutor) { onFaceInfo(processImage(lensFacing, it, bitmap, paint)) }
+                    .addOnFailureListener(cameraExecutor) { LOG.e(it) }
+                    .addOnCompleteListener { imageProxy.close() }
+            }.onFailure { LOG.e(it, it.message) }
+        }
 
 
 }
