2f527d6b94067f7f5d95042a4fb7bd38a0631660
Pedro Lucas Medina Pereira <plmedina2002@gmail.com>
2024-03-30 16:00:44 -0300

    Create Repository.kt

---
+++ app/src/main/java/com/example/facerecognitionandfirebaseapp/data/repositories/Repository.kt
@@ -0,0 +1,8 @@
+package com.example.facerecognitionandfirebaseapp.data.repositories
+
+import android.app.Application
+import com.example.facerecognitionandfirebaseapp.data.database.MainDatabase
+
+class Repository(private val app: Application, private val db: MainDatabase) {
+    
+}
